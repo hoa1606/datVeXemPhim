a51a7e5e0c3c8d20b48afd0fa5617b8213a1fc6f
hoa1606 <dohuuhoa1606@gmail.com>
2024-12-04 03:22:54 +0700

    avatar

--- src/layouts/AdminLayout/_components/Header.jsx
+++ src/layouts/AdminLayout/_components/Header.jsx
@@ -1,14 +1,39 @@
-// src/components/Header.js
-import { AppBar, Toolbar, Typography } from '@mui/material';
-import React from 'react';
+import { AppBar, Avatar, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
+import { logout } from '../../../store/slices/user.slices';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { currentUser } = useSelector((state) => state.user);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    setAnchorEl(null);
+    navigate('/auth/login')
+  };
+
   return (
     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-      <Toolbar>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <Typography variant="h6" noWrap component="div">
           Admin Dashboard
         </Typography>
+        <div>
+          <Avatar onClick={handleMenu} sx={{ cursor: 'pointer' }}>
+            {currentUser?.hoTen?.charAt(0) || '?'}
+          </Avatar>
+          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={() => setAnchorEl(null)}>
+            <MenuItem onClick={handleLogout}>Đăng xuất</MenuItem>
+          </Menu>
+        </div>
       </Toolbar>
     </AppBar>
   );
